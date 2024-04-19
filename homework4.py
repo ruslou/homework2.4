1811de4554db3d77a208ec82254c89e0193aebab
ruslou <165936535+ruslou@users.noreply.github.com>
2024-04-19 16:42:41 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,13 @@
+list_ = ['bmw', 'mb', 'lada', 'kia', 'honda']
+cars_count = 0
+for i in list_:
+    print('я езжу на автомобиле марки', i)
+    cars_count += 10
+    print(cars_count)
+
+
+
+
+
+
+
